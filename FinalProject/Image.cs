795bf3343f1ff1f22093f0c93e235f005f0cc891
shachardavid <shachadavid2013@gmail.com>
2020-12-29 00:04:47 +0200

    fixed bitmap problems

--- FinalProject/Image.cs
+++ FinalProject/Image.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using Android.Content;
 using Android.Content.Res;
 using Android.Graphics;
+using Android.Views;
 
 namespace FinalProject
 {
     public class Image : Shape
     {
+        int height;
+        int width;
 
         public Bitmap image { get; set; }
-        int height; 
-        int width; 
+        public static View View { get; set; }
 
         public Image(float x, float y, int res) : base(x, y, Color.White)
         {
-            this.image = BitmapFactory.DecodeResource(Resources.System, res);
+            image = BitmapFactory.DecodeResource(View.Resources, res);
+            image = image.Copy(Bitmap.Config.Argb8888, true);
+            float ratio = image.Width / image.Height;
+            image = Bitmap.CreateScaledBitmap(image, (int)(200 * ratio), 200, false);
         }
         public override void Draw(Canvas canvas)
         {
