e70b180348d7750b8d2cb5774381687ad07d9598
shachardavid <shachadavid2013@gmail.com>
2020-12-29 17:26:31 +0200

    queen possible moves works

--- FinalProject/Pieces/Queen.cs
+++ FinalProject/Pieces/Queen.cs
@@ -12,7 +12,68 @@ namespace FinalProject.Pieces
 
         public override List<BoardSquare> GetPossiblePlaces(BoardSquare[,] squares)
         {
-            return null;
+            List<BoardSquare> possibilities = new List<BoardSquare>();
+            int x = GetBoardSquareCords(squares)[0],
+                y = GetBoardSquareCords(squares)[1];
+
+            bool first = true, second = true, third = true, fourth = true,
+                 plusx = true, minusx = true, plusy = true, minusy = true;
+            for (int i = 0; i < squares.GetLength(1); i++)
+            {
+                int a, b;
+                if (first)
+                {
+                    a = x + i + 1;
+                    b = y - i - 1;
+                    first = CheckIfPossible(a, b, a < squares.GetLength(0) && b >= 0, squares, possibilities);
+                }
+
+                if (second)
+                {
+                    a = x - i - 1;
+                    b = y - i - 1;
+                    second = CheckIfPossible(a, b, a >= 0 && b >= 0, squares, possibilities);
+                }
+                if (third)
+                {
+                    a = x - i - 1;
+                    b = y + i + 1;
+                    third = CheckIfPossible(a, b, a >= 0 && b < squares.GetLength(1), squares, possibilities);
+                }
+                if (fourth)
+                {
+                    a = x + i + 1;
+                    b = y + i + 1;
+                    fourth = CheckIfPossible(a, b, a < squares.GetLength(0) && b < squares.GetLength(1), squares, possibilities);
+                }
+                if (plusx)
+                {
+                    a = x + i + 1;
+                    b = y;
+                    plusx = CheckIfPossible(a, b, a < squares.GetLength(0), squares, possibilities);
+                }
+
+                if (minusx)
+                {
+                    a = x - i - 1;
+                    b = y;
+                    minusx = CheckIfPossible(a, b, a >= 0, squares, possibilities);
+                }
+                if (plusy)
+                {
+                    a = x;
+                    b = y + i + 1;
+                    plusy = CheckIfPossible(a, b, b < squares.GetLength(1), squares, possibilities);
+                }
+
+                if (minusy)
+                {
+                    a = x;
+                    b = y - i - 1;
+                    minusy = CheckIfPossible(a, b, b >= 0, squares, possibilities);
+                }
+            }
+            return possibilities;
         }
     }
 }
